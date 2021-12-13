76d986dec59730c660449a2ecad335db278f2c52
Arimario Jesus <arimario.jesus@hotmail.com>
2021-12-12 23:43:24 -0300

    fix(config/prismic): document type on linkResolver

--- src/config/prismic.ts
+++ src/config/prismic.ts
@@ -3,7 +3,7 @@ import { Document } from '@prismicio/client/types/documents';
 export const repoName = 'my-blog-ignite';
 
 export const linkResolver = (doc: Document): string => {
-  if (doc.type === 'posts') {
+  if (doc.type === 'post') {
     return `/post/${doc.uid}`;
   }
   return '/';
